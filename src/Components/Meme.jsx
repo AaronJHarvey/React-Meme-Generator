f7c0a5802b73e7d407eb10f925a739a94efeb454
Aaron <Harvey.AaronJ@gmail.com>
2023-01-01 01:32:08 -0500

    Styles Meme Component

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 export default function Meme () {
 	return (
-		<form className='text-form'>
-			<input type='text' className='textbox top-text' placeholder='Top Text' />
-			<input type='text' className='textbox bottom-text' placeholder='Bottom Text' />
-			<button className='submit'>Submit</button>
-		</form>
+		<main>
+			<form className='text-form'>
+				<input type='text' className='textbox top-text' placeholder='Top Text' />
+				<input type='text' className='textbox bottom-text' placeholder='Bottom Text' />
+				<button className='submit'> Get a new meme image 🖼 </button>
+			</form>
+		</main>
 	);
 }
