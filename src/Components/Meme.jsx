915b99209e80bdef426c40c1b69f9add7beb6fa6
Aaron <Harvey.AaronJ@gmail.com>
2023-01-01 00:31:52 -0500

    Begins styling Meme Component

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Meme () {
 	return (
-		<form>
+		<form className='text-form'>
 			<input type='text' className='textbox top-text' placeholder='Top Text' />
 			<input type='text' className='textbox bottom-text' placeholder='Bottom Text' />
 			<button className='submit'>Submit</button>
