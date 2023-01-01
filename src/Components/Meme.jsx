fb93fec74ff45816bf3e74802fa932e68fd0ee07
Aaron <Harvey.AaronJ@gmail.com>
2023-01-01 13:58:48 -0500

    Changes form to div to prevent refresh on click

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 export default function Meme () {
 	return (
 		<main>
-			<form className='text-form'>
+			<div className='meme-form'>
 				<input type='text' className='textbox top-text' placeholder='Top Text' />
 				<input type='text' className='textbox bottom-text' placeholder='Bottom Text' />
 				<button className='submit'> Get a new meme image 🖼 </button>
-			</form>
+			</div>
 		</main>
 	);
 }
