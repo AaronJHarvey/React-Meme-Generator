05c2eee0e7fa1c69702807c879936eea7dcaf1d8
Aaron <Harvey.AaronJ@gmail.com>
2023-01-01 19:10:13 -0500

    Makes button retrieve random meme url when clicked - Does not render the URL to the page yet

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import memesData from '/src/memesData.jsx';
 
 export default function Meme () {
+	// console.log(memesData.data.memes[26].url);
+	let meme;
+
+	function getMeme () {
+		let items = memesData.data.memes;
+		meme = items[Math.floor(Math.random() * items.length)];
+		console.log(meme.url);
+	}
+
 	return (
 		<main>
 			<div className='meme-form'>
 				<input type='text' className='textbox top-text' placeholder='Top Text' />
 				<input type='text' className='textbox bottom-text' placeholder='Bottom Text' />
-				<button className='submit'> Get a new meme image 🖼 </button>
+				<button className='submit' onClick={getMeme}>
+					Get a new meme image 🖼
+				</button>
 			</div>
 		</main>
 	);
